d908cf09c70d4eab62e56b3f6ba160b88f85f55a
Narakamal <kamalmaulana20@gmail.com>
2021-01-20 20:14:28 +0700

    munculin hari dan tanggal DONE

--- tes.php
+++ tes.php
@@ -1,7 +1,6 @@
-<?php 
-$data=['senin','selasa','rabu','kamis'];
-$jam=date('YYYY-MM-DD');
+<?php
+$data = ['senin', 'selasa', 'rabu', 'kamis'];
+$jam = date('YYYY-MM-DD');
 
-<!--Keluarin data mal hari kamis tanggal sekarang-->
-
-?>
+// <!--Keluarin data mal hari kamis tanggal sekarang-->
+echo "hari  $data[3] tanggal $jam ";
